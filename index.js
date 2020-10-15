42929653f34eb735c6ffb726d21d4a9f06a99c50
unknown <you@example.com>
2020-10-15 20:50:03 +0600

    dotenv changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 const port = 2000;
 
 require('dotenv').config();
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nfohl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nfohl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
